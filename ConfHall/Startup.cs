cf8aee0950d261df83da6428f449546eaf409771
idelfonzo.velasquez <idelfonzo.velasquez@hexasystems.com>
2019-01-12 14:54:07 -0400

    Configure Project

--- ConfHall/Startup.cs
+++ ConfHall/Startup.cs
@@ -1,18 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-
-namespace ConfHall
+﻿namespace ConfHall
 {
+
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using System.Text;
+    using ConfHall.Domain.Data;
+    using ConfHall.Domain.Entities;
+    using ConfHall.Domain.Mapper;
+    using ConfHall.Domain.Repositories;
+    using ConfHall.Domain.Services;
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.IdentityModel.Tokens;
+    using Swashbuckle.AspNetCore.Swagger;
+    using ConfHall.Services;
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -26,6 +35,91 @@ namespace ConfHall
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // ===== Add Identity ========           
+            services.AddIdentity<User, Role>(
+                c => { c.User.RequireUniqueEmail = true; }
+            )
+            .AddEntityFrameworkStores<ConfHallDBContext>();
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+           .AddJwtBearer(options =>
+           {
+               options.SaveToken = true;
+               options.TokenValidationParameters = new TokenValidationParameters
+               {
+                   ValidateIssuer = true,
+                   ValidateAudience = true,
+                   ValidateLifetime = true,
+                   ValidateIssuerSigningKey = true,
+
+                   ValidIssuer = Configuration["Tokens:Issuer"],
+                   ValidAudiences = new List<string>
+                   {
+                       "localhost"
+                   },
+                   IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Tokens:Key"]))
+               };
+
+           });
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+
+            #region Services
+
+            services.AddTransient<IPasswordHasher<User>, HashingService>();
+            services.AddTransient<IUserService, UserService>();
+            services.AddTransient<IAccountService, AccountService>();
+            services.AddTransient<IHallService, HallService>();
+
+            #endregion
+            #region Repositories
+            services.AddTransient<IUserRepository, UserRepository>();
+            services.AddTransient<IHallRepository, HallRepository>();
+            #endregion
+
+            ConfigureAutoMapper.Now();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.DescribeAllEnumsAsStrings();
+                options.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info
+                {
+                    Title = "ConfHall HTTP API",
+                    Version = "v1",
+                    Description = "The Conference Hall Restful API ",
+                    TermsOfService = "Terms Of Service"
+                });
+                options.AddSecurityDefinition("Bearer", new ApiKeyScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Name = "Authorization",
+                    In = "header",
+                    Type = "apiKey"
+                });
+            });
+
+            services.AddDbContext<ConfHallDBContext>(options =>
+            {
+                options.UseSqlServer(Configuration["ConnectionStrings:SQLServer"],
+                                        sqlServerOptionsAction: sqlOptions =>
+                                        {
+                                            sqlOptions.MigrationsAssembly(typeof(Startup).GetTypeInfo().Assembly.GetName().Name);
+                                            sqlOptions.EnableRetryOnFailure(maxRetryCount: 10, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
+                                        });
+            });
+
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,12 +129,22 @@ namespace ConfHall
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
+
+            var pathBase = Configuration["PATH_BASE"];
+
+            if (!string.IsNullOrEmpty(pathBase))
             {
-                app.UseHsts();
+                app.UsePathBase(pathBase);
             }
 
-            app.UseHttpsRedirection();
+            app.UseSwagger()
+                .UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint($"{ (!string.IsNullOrEmpty(pathBase) ? pathBase : string.Empty) }/swagger/v1/swagger.json", "Catalog.API V1");
+                    c.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.None);
+                });
+
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
